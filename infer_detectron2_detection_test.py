ef0b1ca05abd97c8201631ec0184650bc53d33d1
ambroise <ambroise.berthe@ikomia.com>
2022-06-23 09:58:59 +0200

    Add test file

---
+++ infer_detectron2_detection_test.py
@@ -0,0 +1,34 @@
+from ikomia.core import task, ParamMap
+import os
+import cv2
+import detectron2
+from ikomia.utils.tests import run_for_test
+from detectron2 import model_zoo
+import logging
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::infer detectron2 detection =====")
+    img = cv2.imread(data_dict["images"]["detection"]["coco"])[::-1]
+    input_img = t.getInput(0)
+    input_img.setImage(img)
+
+    config_paths = os.path.dirname(detectron2.__file__) + "/model_zoo"
+
+    for root, dirs, files in os.walk(config_paths, topdown=False):
+        for name in files:
+            file_path = os.path.join(root, name)
+            possible_cfg = os.path.join(*file_path.split('/')[-2:])
+            if "Detection" in possible_cfg and possible_cfg.endswith('.yaml') and 'rpn' not in possible_cfg:
+                try:
+                    model_zoo.get_checkpoint_url(possible_cfg)
+                except RuntimeError:
+                    continue
+                params = task.get_parameters(t)
+                params["model_name"] = possible_cfg.replace('.yaml', '')
+                # without update = 1, model is not updated between 2 test
+                params["update"] = 1
+                task.set_parameters(t, params)
+                yield run_for_test(t)
+
