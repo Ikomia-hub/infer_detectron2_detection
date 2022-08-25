4b47155083a2dc71db55ed772b8ebefd9d9bd210
ambroise <ambroise.berthe@ikomia.com>
2022-08-25 18:14:30 +0200

    Fix module not found pywin32

--- infer_detectron2_detection_process.py
+++ infer_detectron2_detection_process.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from infer_detectron2_detection import update_path
 from ikomia import core, dataprocess
 import copy
 import os
